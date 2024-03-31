2966b2d9d43cfa84ddd02f647aa39cf1fe2fbc71
Vasiliy Loginevskiy <yeti.or@gmail.com>
2024-03-31 22:54:11 +0300

    chore: fix code style

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -2,8 +2,8 @@
 require('./feedback.css');
 
 // components of feedback page
-const { print: printHeader } = require('../components/Header/header.js');
-const { print: printFooter } = require('../components/Footer/footer.js');
+const { print: printHeader } = require('../components/Header/header');
+const { print: printFooter } = require('../components/Footer/footer');
 
 // logic of feedback page
 console.log('hello feedback.js');
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@
 import './index.css';
 
 // components of main page
-import { print as printHeader } from '../components/Header/header.js';
-import { print as printFooter } from '../components/Footer/footer.js';
+import { print as printHeader } from '../components/Header/header';
+import { print as printFooter } from '../components/Footer/footer';
 
 // logic of main page
 console.log('hello index.js');
@@ -13,6 +13,9 @@ printFooter();
 if (process.env.NODE_ENV === 'development') {
   console.log('Hello dear dev');
 }
+
+// NOTE: VERSION is set inside webpack.config.js
+// eslint-disable-next-line no-undef
 console.log(VERSION);
 console.log(process.env.NODE_ENV);
 // console.log(UPS);
